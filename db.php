d311cd06ab2ea383f6131059ea43e2adde1c5d47
luis sanchez <sasa@sasa-NET.com>
2020-12-31 01:50:02 -0300

    comentario

--- db.php
+++ db.php
@@ -2,11 +2,18 @@
 
 session_start();
 
+
+/*
+	1°servidor
+	2°usuario
+	3°contraseña
+	4°nombre de la BD
+*/
 $conexion = mysqli_connect(
   'localhost',
   'root',
   '',
-  'crud_php_mysql'
+  'id15800448_crud_php_mysql'
 );
 
  ?>
